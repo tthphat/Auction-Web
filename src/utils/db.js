0f2967e2dfaaf51aac6935bb3b28d80e585d0529
23120319 <truongthanhphat1103@gmail.com>
2026-03-20 16:13:12 +0700

    push the original source

---
+++ src/utils/db.js
@@ -0,0 +1,14 @@
+import "dotenv/config"
+
+import knex from 'knex';
+    export default knex({
+      client: 'pg',
+      connection: {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 5432,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME || 'postgres'
+      },
+      pool: { min: 0, max: 7 }
+    });
