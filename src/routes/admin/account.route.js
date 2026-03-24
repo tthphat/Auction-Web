9002a28158865daedfc73c1b1bbe2f513376cf0a
23120327 <thaithienphu74109630@gmail.com>
2026-03-24 15:41:17 +0700

    ref(account): admin account route

--- src/routes/admin/account.route.js
+++ src/routes/admin/account.route.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import { accountController } from '../../controllers/account.controller.js';
 const router = express.Router();
 
 // Define your admin category routes here
-router.get('/profile', (req, res) => {
-    res.render('vwAdmin/account/profile');
-});
+// router.get('/profile', (req, res) => {
+//     res.render('vwAdmin/account/profile');
+// });
+
+router.get('/profile', accountController.getProfile);
 
 export default router;
